ef32401fcdebffc5bbc13c588d8d58b743b092c1
AHerda <adrianherda@gmail.com>
2022-11-14 00:48:26 +0100

    Lista 3 i zadanie 6, 7

---
+++ lista_3/zadanie_6.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	for(int i = 0; i < 256; i += 1) {
+		printf("\x1B[38;5;%dmHello world!\x1B[0m\n", i);
+	}
+	return 0;
+}
+//tak terminal moze wyswietlic wiecej niz 256 kolorow
