3af69c636b9407606e9b44bbb3dcf89ebe7b4201
AHerda <adrianherda@gmail.com>
2022-11-30 02:44:05 +0100

    Zadanie 4 bez plikow ze spacja

--- lista_4/zad4.sh
+++ lista_4/zad4.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find $1 -type f -print0 | xargs -0 -I x bash -c 'echo -n $(sha256sum x) " "; echo $(wc -c x) | cut -d " " -f 1' | sort -nr --key=3 | uniq --check-chars=64 --all-repeated=separate | awk '{print $2 "|" $3}' | column -t -s "|" --table-columns Path,Bytes
+find $1 -type f -print0 | xargs -0 -I x bash -c $'echo -n $(sha256sum  -- "x") " "; echo $(wc -c -- "x") | cut -f1 -d " "' | sort -nr --key=3 | uniq --check-chars=64 -D | cut -c 66- | column -t --table-columns Path,Bytes
