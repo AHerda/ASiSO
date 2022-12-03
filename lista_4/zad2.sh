bfb2666526f716cc91a8336c38388a95499c74d2
AHerda <adrianherda@gmail.com>
2022-12-03 11:08:30 +0100

    Dodane robienie wykresow

--- lista_4/zad2.sh
+++ lista_4/zad2.sh
@@ -1,35 +1,138 @@
 #!/bin/bash
 
+wypiszWykresPobrane () {
+	(for j in {1..10}
+	do
+		local liczba=$(echo "$max_pobrane/10*(10-$j)" | bc)
+		echo -n "$liczba-|"
+		for i in "${pobrane[@]}"
+		do
+			if [[ $i -gt $liczba ]]
+			then
+				echo -en "\e[102m \e[40m"
+			else
+				echo -en "\e[107m \e[40m"
+			fi
+		done
+		echo -en "\n"
+	done
+	echo "|------------------------------") | column -t -s "|" -o "|"
+}
 
-# Wypisywanie uptime
+wypiszWykresWyslane () {
+	(for j in {1..10}
+	do
+		local liczba=$(echo "$max_wyslane/10*(10-$j)" | bc)
+		echo -n "$liczba-|"
+		for i in "${wyslane[@]}"
+		do
+			if [[ $i -gt $liczba ]]
+			then
+				echo -en "\e[102m \e[40m"
+			else
+				echo -en "\e[107m \e[40m"
+			fi
+		done
+		echo -en "\n"
+	done
+	echo "|------------------------------") | column -t -s "|" -o "|"
+}
+
+declare -a pobrane
+declare -a wyslane
+declare -i iwyslane
+declare -i ipobrane
+declare -i ipobrane_stare
+declare -i iwyslane_stare
+
+ipobrane_stare=$(cat /proc/net/dev | grep "enp0" | awk '{print $2}')
+iwyslane_stare=$(cat /proc/net/dev | grep "enp0" | awk -v x=10 '{print $x}')
+
+for i in  {0..29}
+do
+	pobrane[i]=0
+	wyslane[i]=0
+done
+
 
 linia="---------------------------------------------|--------"
-uptime=$(cat /proc/uptime | awk '{print $1}')
-(echo -e "\e[33mUpTime\e[37m"
-echo "Days:|" $(echo "$uptime/86400" | bc)
-echo "Hours:|" $(echo "$uptime%86400/3600" | bc)
-echo "Minute:|" $(echo "$uptime%86400%3600/60" | bc)
-echo "Seconds:|" $(echo "$uptime%86400%3600%60" | bc)
+while true
+do
+	clear
 
-echo "$linia"
+	# Wypisywanie uptime
+	
+	uptime=$(cat /proc/uptime | awk '{print $1}')
+	(echo -e "\e[33mUpTime\e[37m"
+	echo "Days:|" $(echo "$uptime/86400" | bc)
+	echo "Hours:|" $(echo "$uptime%86400/3600" | bc)
+	echo "Minute:|" $(echo "$uptime%86400%3600/60" | bc)
+	echo "Seconds:|" $(echo "$uptime%86400%3600%60" | bc)
 
-# Wypisywanie baterii
+	echo "$linia"
 
-echo -n "Bateria:|"
-baterie=$(cat /sys/class/power_supply/BAT0/uevent | grep "POWER_SUPPLY_ENERGY_FULL=" | awk -F "=" '{print $2}')
-actual_baterie=$(cat /sys/class/power_supply/BAT0/uevent | grep "POWER_SUPPLY_ENERGY_NOW=" | awk -F "=" '{print $2}')
-sto=100
-echo -e $(echo "scale=1 ; $actual_baterie/$baterie*100" | bc -l) "%\n$linia"
+	# Wypisywanie baterii
 
-# Wypisywanie obiciazenie systemu
+	echo -n "Bateria:|"
+	baterie=$(cat /sys/class/power_supply/BAT0/uevent | grep "POWER_SUPPLY_ENERGY_FULL=" | awk -F "=" '{print $2}')
+	actual_baterie=$(cat /sys/class/power_supply/BAT0/uevent | grep "POWER_SUPPLY_ENERGY_NOW=" | awk -F "=" '{print $2}')
+	sto=100
+	echo -e $(echo "scale=1 ; $actual_baterie/$baterie*100" | bc -l) "%\n$linia"
 
-echo -e "\e[33mSrednie obciazenie systemu\e[37m"
-echo "Z ostatniej minuty:|" $(cat /proc/loadavg | awk '{print $1}')
-echo "Z ostatnich 5 minut:|" $(cat /proc/loadavg | awk '{print $2}')
-echo -e "Z ostatnich 15 minut:|" $(cat /proc/loadavg | awk '{print $3}') "\n$linia"
+	# Wypisywanie obiciazenie systemu
 
-# Wypisywanie pamieci
+	echo -e "\e[33mSrednie obciazenie systemu\e[37m"
+	echo "Z ostatniej minuty:|" $(cat /proc/loadavg | awk '{print $1}')
+	echo "Z ostatnich 5 minut:|" $(cat /proc/loadavg | awk '{print $2}')
+	echo -e "Z ostatnich 15 minut:|" $(cat /proc/loadavg | awk '{print $3}') "\n$linia"
 
-mem_total=$(cat /proc/meminfo | grep "^MemTotal" | awk '{print $2}')
-mem=$(cat /proc/meminfo | grep "^MemFree" | awk '{print $2}')
-echo -e "Aktualne wykorzystanie pamieci:|" $(echo "scale=1; ($mem_total-$mem)/$mem_total*100" | bc -l) "%") | column -t -s "|" -o "|"
+	# Wypisywanie pamieci
+
+	mem_total=$(cat /proc/meminfo | grep "^MemTotal" | awk '{print $2}')
+	mem=$(cat /proc/meminfo | grep "^MemFree" | awk '{print $2}')
+	echo -e "Aktualne wykorzystanie pamieci:|" $(echo "scale=1; ($mem_total-$mem)/$mem_total*100" | bc -l) "%") | column -t -s "|" -o "|"
+	
+	#
+	# Miejsce na oblicznie cpu
+	#
+	
+	# Wykres danych z sieci
+
+	echo -e "\n\e[33mWykres danych sieci\e[37m"
+
+	ipobrane=$(cat /proc/net/dev | grep "enp0" | awk '{print $2}')
+	iwyslane=$(cat /proc/net/dev | grep "enp0" | awk -v x=10 '{print $x}')
+
+	max_pobrane=0
+	max_wyslane=0
+	for i in {29..0}
+	do
+		if [ $i -ne 0 ]
+		then
+			pobrane[i]=${pobrane[i-1]}
+			wyslane[i]=${wyslane[i-1]}
+		else
+			pobrane[i]=$(echo "$ipobrane-$ipobrane_stare" | bc)
+			wyslane[i]=$(echo "$iwyslane-$iwyslane_stare" | bc)
+		fi
+		
+		if [[ ${pobrane[i]} -gt $max_pobrane ]]
+		then
+			max_pobrane=${pobrane[i]}
+		fi
+
+		if [[ ${wyslane[i]} -gt $max_wyslane ]]
+		then
+			max_wyslane=${wyslane[i]}
+		fi
+	done
+
+	echo -e "\e[33m	Wykres danych pobranych\e[37m"
+	wypiszWykresPobrane
+	echo -e "\e[33m	Wykres danych wyslanych\e[37m"
+	wypiszWykresWyslane
+	
+	ipobrane_stare=$ipobrane
+	iwyslane_stare=$iwyslane
+	sleep 1
+done
