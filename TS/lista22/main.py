865eae241ec467c1345e874e266701944feaf7f0
AHerda <adrianherda@gmail.com>
2023-04-27 23:53:27 +0200

    zrobione wszytskie testy

--- TS/lista22/main.py
+++ TS/lista22/main.py
@@ -1,17 +1,22 @@
 from util import *
-import numpy as np
+from test1 import test1
+from test2 import test2
+from test3 import test3
 
 n = 20
 
 def main():
     graph = make_graph()
     matrix = make_matrix(n, 5)
+    #generate_capacity(graph, matrix, 8)
+    #generate_flow(graph, matrix)
     draw_graph(graph)
     print(matrix)
+    print(calculate_reliability(graph, matrix, 0.9, 0.2, 128))
 
-    #test_changing_N(0.8, 0.1, 128)
-    #test_changing_capacity(0.8, 0.1, 128, 1.0625)
-    #test_adding_edges(0.8, 0.1, 128)
+    #print(test1(0.8, 0.1, 128, True))
+    #print(test2(0.8, 0.1, 128, 1.1, True))
+    #print(test3(0.8, 0.1, 128, True))
 
 if __name__ == "__main__":
     main()
--- TS/lista22/util.py
+++ TS/lista22/util.py
@@ -5,7 +5,6 @@ import numpy as np
 
 edg = []
 
-
 #funckja generujaca dodecahedron
 def make_graph(v = 20):
     G = nx.Graph()
@@ -26,7 +25,7 @@ def make_graph(v = 20):
         i += 1
         j += 2
     for i, j in edges_temp:
-        G.add_edge(i, j)
+        G.add_edge(i, j, a = 0, c = 0)
     
     global edg
     edg = edges_temp
@@ -36,10 +35,11 @@ def make_graph(v = 20):
 #funkcja tworzy wizualizacje grafu
 def draw_graph(graph):
     plt.figure(figsize=(8, 8))
+    plt.title("Wzór grafu")
     pos = nx.shell_layout(graph, (list(range(5)), list(range(5, 15)), list(range(15, 20))), .4)
     #pos = nx.spring_layout(graph)
-    nx.draw_networkx_edge_labels(graph, pos, font_size=6, bbox=dict(fc = "w", linewidth = 0, alpha=1.0))
     nx.draw_networkx_labels(graph, pos, font_color="w")
+    #nx.draw_networkx_edge_labels(graph, pos, font_size=6, font_color="r", bbox=dict(fc = "w", linewidth = 0, alpha=1.0))
     nx.draw(graph, pos)
     plt.savefig("lista22/wykresy/Graf.png", dpi=400)
 
@@ -55,75 +55,82 @@ def make_matrix(v, max):
     return matrix
 
 #funkcja generujaca przeplyw, wraca true jezeli z kazdego wierzcholka mozna wszedzie wyslac okreslona w macierzy natezen liczbe pakietow
-def generate_flow(graph, N):
-    for i in range(len(N)):
-        for j in range(len(N)):
-            if N[i][j] != 0:
-                path = generate_path(graph, i, j, N[i][j]) #generowanie sciezki z wierzcholka i do j
+def generate_flow(graph, matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            if matrix[i][j] != 0:
+                path = generate_path(graph, i, j, matrix[i][j])
                 if len(path) == 0:
-                    return False #jezeli nie udalo sie przeslac pakietow to wracamy false
+                    return False
                 for k in range(0, len(path)-1):
-                    graph[path[k]][path[k+1]]['taken'] += N[i][j] #zwiekszenie przeplywu na kazdej krawedzi na trasie o liczbe przesylanych pakietow
+                    graph[path[k]][path[k+1]]["a"] += matrix[i][j]
     return True
 
 #funkcja generujaca przepustowosc sieci
-def generate_capacity(graph, N, m):
-    for i in range(len(N)):
-        max_size = max(N[i])
+def generate_capacity(graph, matrix, m):
+    for i in range(len(matrix)):
+        max_size = max(matrix[i])
         for edge in graph.edges(i, data = True):
-            edge[2]['capacity'] = max_size * len(N)**2 * m * uniform(2.0, 3.0)
+            edge[2]["c"] = int(max_size * len(matrix)**2 * m * uniform(2.0, 3.0))
 
 #funkcja generujaca sciezke package pakietow z wierzcholka i do j
 def generate_path(graph, i, j, package):
     try:
-        path = nx.dijkstra_path(graph,i,j) #najpierw znajdujemy najkrotsza sciezke algorytmem dijksty
+        path = nx.dijkstra_path(graph, i,  j)
     except:
-        return [] #jezeli nie istnieje to znaczy, ze graf sie rozspojnil
+        return []
     if_ok = False
-    gen = nx.all_simple_paths(graph, source = i, target = j) #generator wracajacy wszystkie sciezki z i do j
+    gen = nx.all_simple_paths(graph, source = i, target = j)
     while not if_ok:
         if_ok = True
         for k in range(0, len(path)-1):
-            if graph[path[k]][path[k+1]]['taken'] + package > graph[path[k]][path[k+1]]['capacity']: #sprawdzamy czy pakiety 'zmieszcza sie' na wybranej trasie
-                if_ok = False 
+            if graph[path[k]][path[k+1]]["a"] + package > graph[path[k]][path[k+1]]["c"]:
+                if_ok = False
                 try:
-                    path = next(gen) #jezeli sie nie mieszcza to bierzemy kolejna trase
+                    path = next(gen)
                 except:
-                    path = [] #jezeli wyczerpalismy wszystkie sciezki to znaczy, ze nie mozna przeslac pakietow z i do j
+                    path = []
                 break
     return path
 
 #funkcja liczaca opoznienie pakietow wedlug wzoru z polecenia
-def calculate_delay(graph, N, m):
+def calculate_delay(graph, matrix, m):
     G = 0
-    for row in N:
+    for row in matrix:
         for num in row:
             G += num
     sum_e = 0
-    for i in range(len(N)):
+    for i in range(len(matrix)):
         for edge in graph.edges(i, data = True):
-            term = edge[2]['taken']/(edge[2]['capacity']/m-edge[2]['taken']) #liczenie wyrazenia dla kazdej krawedzi
+            term = edge[2]["a"]/(edge[2]["c"]/m-edge[2]["a"]) #liczenie wyrazenia dla kazdej krawedzi
             if term < 0: #jezeli wyszlo ujemne to znaczy, ze krawedz zostala przeciazona i nalezy wrocic fail
                 return -1
             else:
                 sum_e += term
-    return 1/G * sum_e
+    return sum_e / G
 
 #funkcja liczace miare niezawodnosci sieci
-def calculate_reliability(graph, N, p, T_max, m):
+def calculate_reliability(graph, matrix, p, T_max, m, iterations=200, intervals=5):
     counter = 0
-    for _ in range(1000):
-        helper = graph.copy() #tworzenie kopii grafu wejsciowego, na ktorej bedzie prowadzona proba
-        to_del = []
-        for edge in graph.edges:
-            i = uniform(0.0,1.0) #dla kazdej krawedzi losujemy czy zostatnie usunieta
-            if i > p:
-                to_del.append(edge)
-        for edge in to_del:
-            helper.remove_edge(*edge) #usuwamy wybrane krawedzi
-        if_connected = generate_flow(helper, N) #generujemy na nowo przeplyw
-        if if_connected: #jezeli nie doszlo do rozspojnienia to przeprowadzamy test
-            T = calculate_delay(helper, N, m)
-            if T < T_max and T > 0: 
-                counter += 1
-    return counter/1000
+    for i in range(iterations):
+        graph_temp = graph.copy()
+        for j in range(intervals):
+            to_del = []
+
+            for edge in graph_temp.edges:
+                i = uniform(0.0,1.0)
+                if i > p:
+                    to_del.append(edge)
+        
+            for edge in to_del:
+                graph_temp.remove_edge(*edge)
+            
+            connected = generate_flow(graph_temp, matrix)
+
+            if connected:
+                T = calculate_delay(graph_temp, matrix, m)
+                if T < T_max and T > 0: 
+                    counter += 1
+            else:
+                break
+    return counter / (iterations * intervals)
